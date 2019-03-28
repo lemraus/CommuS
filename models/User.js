238c366cdccf314f69fc694564e3a423755888dc
Chausse Armel <armel.chausse@student.ecp.fr>
2019-03-28 02:25:52 +0100

    Switching from bcrypt to bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import { Schema } from "mongoose";
 import mongoose from "../database/mongoDB";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema({
     username: {
