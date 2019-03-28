5ebb4bcd0ac0c0ba93fa972cd6e6de976dde8354
Chausse Armel <armel.chausse@student.ecp.fr>
2019-03-28 05:32:12 +0100

    Connection: Keep-Alive

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app
         res.header("Access-Control-Allow-Origin", "http://commus.fr");
         res.header("Access-Control-Allow-Methods", "DELETE");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header("Connection", "Keep-Alive");
         next();
     });
 
